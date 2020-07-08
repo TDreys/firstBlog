7ceba4bd0b8ec41c6d8bcf64bff2730b50f0fbec
Tom <txd844@student.bham.ac.uk>
2020-07-08 22:54:58 +0100

    first test

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,17 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://127.0.0.1:8000')
+class NewVisitorTest(unittest.TestCase):
+	def setUp(self):
+		self.browser = webdriver.Firefox()
 
-assert 'Django' in browser.title
+	def tearDown(self):
+		self.browser.quit()
+
+	def test_can_open_page(self):
+		self.browser.get('http://127.0.0.1:8000')
+		self.assertIn('Django', self.browser.title) 
+		self.fail('finish test')
+
+if __name__ == '__main__':  
+    unittest.main(warnings='ignore')
